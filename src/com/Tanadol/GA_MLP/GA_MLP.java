7fa4d34cacac0dd569c4cbf86f79cb05f6cfe2b2
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2022-10-16 03:23:41 +0700

    add: fitness eval,ga,selection,init pop.,refactor mlp to train with ga

---
+++ src/com/Tanadol/GA_MLP/GA_MLP.java
@@ -0,0 +1,89 @@
+package com.Tanadol.GA_MLP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GA_MLP {
+    private static final Random random = new Random();
+    private final int populationSize = 50;
+
+    private Individual[] population = new Individual[populationSize];
+    private double totalFitness;
+
+    private double[] input;
+    private double[] desireOutput;
+
+    public void run(int maxGeneration) {
+        initPopulation();
+
+        int gen = 0;
+        while (gen < maxGeneration) {
+            evaluateFitness();
+            select();
+            crossOver();
+            mutation();
+            gen++;
+        }
+    }
+
+    private void initPopulation() {
+        for (int i = 0; i < population.length; i++) {
+            population[i] = new Individual();
+        }
+    }
+
+    private void evaluateFitness() {
+        totalFitness = 0;
+        for (Individual p : population) {
+            p.fitness = 1.0 - p.network.feedForward(input, desireOutput);
+            totalFitness += p.fitness;
+        }
+    }
+
+    private void select() {
+        List<Individual> newPopulation = new ArrayList<>(populationSize);
+        // Using Roulette Wheel Selection
+        // Find selection prob. and init cumulative prob.
+        int n = population.length;
+        double[] cumulativeProb = new double[n];
+        for (int i = 0; i < n; i++) {
+            population[i].selectProb = population[i].fitness / totalFitness;
+            if (i == 0) {
+                cumulativeProb[i] = population[i].selectProb;
+            } else {
+                cumulativeProb[i] = population[i].selectProb + cumulativeProb[i - 1];
+            }
+        }
+
+        // Apply selection operator n times
+        for (int i = 0; i < n; i++) {
+            double rand = random.nextDouble();
+
+            if (0.0 <= rand || rand <= cumulativeProb[0]) newPopulation.add(population[0]);
+
+            for (int j = 1; j < n; j++) {
+                if (cumulativeProb[j - 1] <= rand || rand <= cumulativeProb[j])
+                    newPopulation.add(population[j]);
+            }
+        }
+
+        // Selection for replacement
+        int currentSize = newPopulation.size();
+        while (currentSize < populationSize) {
+            int randIdx = random.nextInt(populationSize);
+            newPopulation.add(population[randIdx]);
+            currentSize++;
+        }
+
+        population = newPopulation.toArray(new Individual[populationSize]);
+    }
+
+    private void crossOver() {
+        // TODO: Cross Chromosome, Update weights
+    }
+
+    private void mutation() {
+        // TODO: Mutate chromosome, Update weights
+    }
+}
---
+++ src/com/Tanadol/GA_MLP/Individual.java
@@ -0,0 +1,57 @@
+package com.Tanadol.GA_MLP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Individual {
+    private static final MathFunction leakyReluFn = (x) -> {
+        if (x <= 0) return 0.01 * x;
+        else return x;
+    };
+
+    private static final MathFunction sigmoidFn = (x) -> 1.0 / (1.0 + Math.exp(x));
+
+    private static final double minWeight = 0.0;
+    private static final double maxWeight = 1.0;
+    private static final int[] nodes = new int[]{30, 16, 1};
+
+    protected Network network;
+    protected List<Double> chromosome = new ArrayList<>();
+    protected double fitness;
+    protected double selectProb;
+
+    public Individual() {
+        Matrix[] biases = new Matrix[2];
+        biases[0] = new Matrix(16, 1);
+        biases[1] = new Matrix(1, 1);
+        network = new Network(nodes, leakyReluFn, sigmoidFn, minWeight, maxWeight, biases);
+        updateChromosome();
+    }
+
+    protected void updateNewWeights() {
+        Matrix[] newWeights = new Matrix[2];
+        int c = 0;
+        for (int l = 0; l < nodes.length - 1; l++) {
+            newWeights[l] = new Matrix(nodes[l], nodes[l + 1]);
+            for (int i = 0; i < newWeights[l].getRows(); i++) {
+                for (int j = 0; j < newWeights[l].getCols(); j++) {
+                    newWeights[l].data[i][j] = chromosome.get(c);
+                    c++;
+                }
+            }
+        }
+        network.weights = newWeights;
+        updateChromosome();
+    }
+
+    private void updateChromosome() {
+        chromosome.clear();
+        for (Matrix m : network.weights) {
+            for (int i = 0; i < m.getRows(); i++) {
+                for (int j = 0; j < m.getCols(); j++) {
+                    chromosome.add(m.data[i][j]);
+                }
+            }
+        }
+    }
+}
