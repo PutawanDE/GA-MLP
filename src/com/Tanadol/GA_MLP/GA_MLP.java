1ee904633ff6ac33291715b48f847eb966f15448
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2022-10-16 22:44:40 +0700

    add: crossover, mutation

--- src/com/Tanadol/GA_MLP/GA_MLP.java
+++ src/com/Tanadol/GA_MLP/GA_MLP.java
@@ -1,33 +1,39 @@
 package com.Tanadol.GA_MLP;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class GA_MLP {
     private static final Random random = new Random();
-    private final int populationSize = 50;
 
-    private Individual[] population = new Individual[populationSize];
+    private int populationSize;
+    private double crossOverRate;
+
+    private Individual[] population;
     private double totalFitness;
 
     private double[] input = new double[30];
     private double[] desireOutput = new double[1];
 
-    public void run(int maxGeneration) {
+    public Individual[] run(int maxGeneration, int populationSize, double crossoverRate, int crossoverPoint,
+                            double mutationProb, double mutateMin, double mutateMax) {
+        this.populationSize = populationSize;
+        this.crossOverRate = crossoverRate;
+
         initPopulation();
 
         int gen = 0;
         while (gen < maxGeneration) {
             evaluateFitness();
             select();
-            crossOver();
-            mutation();
+            crossover(crossoverPoint);
+            mutation(mutationProb, mutateMin, mutateMax);
             gen++;
         }
+        return population;
     }
 
     private void initPopulation() {
+        population = new Individual[populationSize];
         for (int i = 0; i < population.length; i++) {
             population[i] = new Individual();
         }
@@ -42,7 +48,7 @@ public class GA_MLP {
     }
 
     private void select() {
-        List<Individual> newPopulation = new ArrayList<>(populationSize);
+        List<Individual> selected = new ArrayList<>(populationSize);
         // Using Roulette Wheel Selection
         // Find selection prob. and init cumulative prob.
         int n = population.length;
@@ -61,11 +67,11 @@ public class GA_MLP {
             double rand = random.nextDouble();
 
             if (0.0 <= rand || rand <= cumulativeProb[0]) {
-                newPopulation.add(population[0]);
+                selected.add(population[0]);
             } else {
                 for (int j = 1; j < n; j++) {
                     if (cumulativeProb[j - 1] <= rand || rand <= cumulativeProb[j]) {
-                        newPopulation.add(population[j]);
+                        selected.add(population[j]);
                         break;
                     }
                 }
@@ -73,21 +79,87 @@ public class GA_MLP {
         }
 
         // Selection for replacement
-        int currentSize = newPopulation.size();
+        int currentSize = selected.size();
         while (currentSize < populationSize) {
             int randIdx = random.nextInt(populationSize);
-            newPopulation.add(population[randIdx]);
+            selected.add(population[randIdx]);
             currentSize++;
         }
 
-        population = newPopulation.toArray(new Individual[populationSize]);
+        population = selected.toArray(new Individual[populationSize]);
     }
 
-    private void crossOver() {
-        // TODO: Cross Chromosome, Update weights
+    // N-point Cross-Over
+    private void crossover(int n) {
+        List<Individual> newPopulation = new ArrayList<>(populationSize);
+        List<Individual> matingPool = new ArrayList<>();
+        int lastInMatingPool = 0;
+        boolean[] isParent = new boolean[populationSize];
+
+        for (int i = 0; i < populationSize; i++) {
+            double rand = random.nextDouble();
+            if (rand < crossOverRate) {
+                matingPool.add(population[i]);
+                lastInMatingPool = i;
+                isParent[i] = true;
+            }
+        }
+
+        int m = matingPool.size();
+        if (m % 2 != 0) {
+            isParent[lastInMatingPool] = false;
+            matingPool.remove(m - 1);
+            m--;
+        }
+        Collections.shuffle(matingPool);
+
+        for (int i = 0; i < m; i += 2) {
+            int randParent1 = i;
+            int randParent2 = i + 1;
+
+            Individual parent1 = matingPool.get(randParent1);
+            Individual parent2 = matingPool.get(randParent2);
+            Individual offspring1 = new Individual();
+            Individual offspring2 = new Individual();
+
+            int[] crossingSites = new int[n];
+            for (int j = 0; j < n; j++) {
+                crossingSites[j] = random.nextInt(parent1.chromosome.size());
+            }
+            Arrays.sort(crossingSites);
+
+            for (int j = 1; j < n; j++) {
+                if (j % 2 == 0) {
+                    offspring1.chromosome.addAll(parent1.chromosome.subList(crossingSites[j - 1], crossingSites[j]));
+                    offspring2.chromosome.addAll(parent2.chromosome.subList(crossingSites[j - 1], crossingSites[j]));
+                } else {
+                    offspring1.chromosome.addAll(parent2.chromosome.subList(crossingSites[j - 1], crossingSites[j]));
+                    offspring2.chromosome.addAll(parent1.chromosome.subList(crossingSites[j - 1], crossingSites[j]));
+                }
+            }
+
+            offspring1.updateNewWeights();
+            offspring2.updateNewWeights();
+            newPopulation.add(offspring1);
+            newPopulation.add(offspring2);
+        }
+
+        for (int i = 0; i < populationSize; i++) {
+            if (!isParent[i]) newPopulation.add(population[i]);
+        }
+
+        population = newPopulation.toArray(new Individual[0]);
     }
 
-    private void mutation() {
-        // TODO: Mutate chromosome, Update weights
+    private void mutation(double mutationProb, double min, double max) {
+        for (Individual p : population) {
+            for (int i = 0; i < p.chromosome.size(); i++) {
+                double q = random.nextDouble();
+                if (q < mutationProb) {
+                    p.chromosome.set(i, random.nextDouble(min, max));
+                    p.updateNewWeights();
+                }
+            }
+        }
     }
 }
--- src/com/Tanadol/GA_MLP/Main.java
+++ src/com/Tanadol/GA_MLP/Main.java
@@ -3,6 +3,8 @@ package com.Tanadol.GA_MLP;
 public class Main {
     public static void main(String[] args) {
         GA_MLP ga = new GA_MLP();
-        ga.run(50);
+        Individual[] solution = ga.run(100, 50, 0.3, 3,
+                0.001, -1.0, 1.0);
+        System.out.println(solution[0].fitness);
     }
 }
